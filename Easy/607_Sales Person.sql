c5441a2f786f45b2697648e12dec28a977bfa1f3
HarshDeepYadav <harshyadavdeep452@gmail.com>
2026-07-21 15:06:51 -0600

    Solved problem #607 Sales Person

---
+++ Easy/607_Sales Person.sql
@@ -0,0 +1,11 @@
+-- Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
+
+-- Return the result table in any order.
+
+select s.name from SalesPerson s JOIN Orders o ON s.sales_id = o.sales_id JOIN Company c on (o.com_id = c.com_id and c.name = 'RED') 
+where o.sales_id is null
+
+select salesperson.name
+from orders o join company c on (o.com_id = c.com_id and c.name = 'RED')
+right join salesperson on salesperson.sales_id = o.sales_id
+where o.sales_id is null
