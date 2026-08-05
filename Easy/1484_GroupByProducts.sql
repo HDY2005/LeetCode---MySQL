3349570a9b8e21a548134ae84f8d8a673992b7d3
HarshDeepYadav <harshyadavdeep452@gmail.com>
2026-08-05 17:09:40 -0600

    Optimization

--- Easy/1484_GroupByProducts.sql
+++ Easy/1484_GroupByProducts.sql
@@ -1,7 +1,12 @@
-SELECT
-    sell_date,
-    COUNT(DISTINCT product) AS num_sold,
-    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+-- Write a solution to find for each date the number of different products sold and their names.
+
+-- The sold products names for each date should be sorted lexicographically.
+
+-- Return the result table ordered by sell_date.
+
+-- The result format is in the following examp
+
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
 FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date;
