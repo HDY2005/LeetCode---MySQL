9f45349e119ea5a9f957badaa254175d7b6db84c
HarshDeepYadav <harshyadavdeep452@gmail.com>
2026-08-13 16:41:31 -0600

    SQL Question solving

--- Hard/601_HumanTraffic.sql
+++ Hard/601_HumanTraffic.sql
@@ -1,15 +1,14 @@
+-- Write a solution to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
 
+-- Return the result table ordered by visit_date in ascending order.
 
-SELECT id, visit_date, people
-FROM (
+SELECT id, visit_date, people FROM (
     SELECT *,
            id - ROW_NUMBER() OVER (ORDER BY id) AS grp
     FROM Stadium
     WHERE people >= 100
 ) t
-WHERE grp IN (
-    SELECT grp
-    FROM (
+WHERE grp IN ( SELECT grp FROM (
         SELECT id - ROW_NUMBER() OVER (ORDER BY id) AS grp
         FROM Stadium
         WHERE people >= 100
